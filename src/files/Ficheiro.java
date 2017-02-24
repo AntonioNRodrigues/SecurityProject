dcfe25927e456a82b41ffbc461f922ba29d36d0a
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-02-24 11:35:02 +0000

    getters and setters and way to compare 2 ficheiros

--- src/files/Ficheiro.java
+++ src/files/Ficheiro.java
@@ -1,11 +1,42 @@
 package files;
 
+import java.io.File;
+import java.io.Serializable;
 import java.sql.Timestamp;
 
-import server.repository.RemoteRepository;
-
-public class Ficheiro {
+public class Ficheiro implements Comparable<Timestamp>, Serializable {
+	private static final long serialVersionUID = 1L;
 	private String nameFile;
 	private Timestamp timestamp;
-	private RemoteRepository repository;
+	private File file;
+
+	public String getNameFile() {
+		return nameFile;
+	}
+
+	public void setNameFile(String nameFile) {
+		this.nameFile = nameFile;
+	}
+
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	@Override
+	public int compareTo(Timestamp o) {
+		return this.timestamp.compareTo(o);
+	}
+
 }
