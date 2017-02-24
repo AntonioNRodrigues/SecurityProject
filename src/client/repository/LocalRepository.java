7c1ca9a58655e7071e9f580fad7226cd7a513c42
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-02-24 16:06:50 +0000

    added the getters and setters for the classes

--- src/client/repository/LocalRepository.java
+++ src/client/repository/LocalRepository.java
@@ -12,4 +12,36 @@ public class LocalRepository {
 	private String nameRepo;
 	private List<Ficheiro> listFiles;
 	
+	public User getOnwer() {
+		return onwer;
+	}
+
+	public void setOnwer(User onwer) {
+		this.onwer = onwer;
+	}
+
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getNameRepo() {
+		return nameRepo;
+	}
+
+	public void setNameRepo(String nameRepo) {
+		this.nameRepo = nameRepo;
+	}
+
+	public List<Ficheiro> getListFiles() {
+		return listFiles;
+	}
+
+	public void setListFiles(List<Ficheiro> listFiles) {
+		this.listFiles = listFiles;
+	}
+
 }
--- src/user/User.java
+++ src/user/User.java
@@ -1,6 +1,32 @@
 package user;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private String name;
 	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User(String name, String password) {
+		super();
+		this.name = name;
+		this.password = password;
+	}
+
 }
