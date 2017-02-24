ab4e0395c036b44b93bf66e80ac87677873cc084
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-02-24 11:34:13 +0000

    minor stuff

--- src/message/MessageFactory.java
+++ src/message/MessageFactory.java
@@ -8,14 +8,12 @@ public class MessageFactory {
 
 	public Message getMessage(User localUser, String serverAdress, String password) {
 		return new Message(localUser, serverAdress, password);
-
 	}
 
 	public MessageP getMessageP(User localUser, String serverAdress, String password, String operation,
 			String repo_file_name, String typeSend) {
 		return new MessageP(localUser, serverAdress, password, typeSend(typeSend), repo_file_name,
 				operation(operation));
-
 	}
 
 	public MessageRS getMessageRS(User localUser, String serverAdress, String password, String opertaion,
