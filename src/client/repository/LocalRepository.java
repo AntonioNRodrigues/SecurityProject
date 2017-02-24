d12fa666ac129a243499b70fb017091f827f7ee1
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-02-24 11:16:10 +0000

    defining the structure of local repository

--- src/client/repository/LocalRepository.java
+++ src/client/repository/LocalRepository.java
@@ -1,5 +1,15 @@
 package client.repository;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import files.Ficheiro;
+import user.User;
+
 public class LocalRepository {
+	private User onwer;
+	private Timestamp timestamp;
+	private String nameRepo;
+	private List<Ficheiro> listFiles;
 	
 }
