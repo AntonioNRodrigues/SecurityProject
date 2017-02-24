dab6b3e84352df79537b6b21b7069e82fd4f6a60
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-02-24 10:08:53 +0000

    remote repository to use from the point of view of the server.

--- src/server/repository/RemoteRepository.java
+++ src/server/repository/RemoteRepository.java
@@ -1,16 +1,46 @@
-package repository;
+package server.repository;
 
 import java.sql.Timestamp;
 import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
 
 import files.Ficheiro;
-import users.User;
+import user.User;
 
-public class Repository {
+public class RemoteRepository {
 	private User onwer;
 	private Timestamp timestamp;
 	private String nameRepo;
-	private Map<String, Ficheiro> mapFiles;
+	private Map<String, Set<Ficheiro>> mapFiles;
+	public User getOnwer() {
+		return onwer;
+	}
+	public void setOnwer(User onwer) {
+		this.onwer = onwer;
+	}
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getNameRepo() {
+		return nameRepo;
+	}
+	public void setNameRepo(String nameRepo) {
+		this.nameRepo = nameRepo;
+	}
+	public Map<String, Set<Ficheiro>> getMapFiles() {
+		return mapFiles;
+	}
+	public void setMapFiles(Map<String, Set<Ficheiro>> mapFiles) {
+		this.mapFiles = mapFiles;
+	}
+	public Set<Ficheiro> getVersionList(String nameFile){
+		return mapFiles.get(nameFile);
+	}
+	
 	
 	
 }
