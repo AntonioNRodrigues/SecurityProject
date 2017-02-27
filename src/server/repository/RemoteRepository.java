265cdcc63801b2169bd27affa007af2c41f5482a
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-02-27 14:47:31 +0000

    changed the Timestamp to a long representation of the timestamp. This change was made because a file has the propriety lastmodified (long).

--- src/server/repository/RemoteRepository.java
+++ src/server/repository/RemoteRepository.java
@@ -1,47 +1,67 @@
 package server.repository;
 
-import java.sql.Timestamp;
 import java.util.Map;
 import java.util.Set;
-import java.util.SortedSet;
 
 import files.Ficheiro;
-import user.User;
 
 public class RemoteRepository {
-	private User onwer;
-	private Timestamp timestamp;
+	private String onwer;
+	private Long timestamp;
 	private String nameRepo;
 	private Map<String, Set<Ficheiro>> mapFiles;
-	
-	public User getOnwer() {
+
+	public RemoteRepository( Long timestamp, String nameRepo) {
+		super();
+		this.timestamp = timestamp;
+		this.nameRepo = nameRepo;
+	}
+	public RemoteRepository(String onwer, Long timestamp, String nameRepo) {
+		super();
+		this.onwer = onwer;
+		this.timestamp = timestamp;
+		this.nameRepo = nameRepo;
+	}
+
+	public String getOnwer() {
 		return onwer;
 	}
-	public void setOnwer(User onwer) {
+
+	public void setOnwer(String onwer) {
 		this.onwer = onwer;
 	}
-	public Timestamp getTimestamp() {
+
+	public Long getTimestamp() {
 		return timestamp;
 	}
-	public void setTimestamp(Timestamp timestamp) {
+
+	public void setTimestamp(Long timestamp) {
 		this.timestamp = timestamp;
 	}
+
 	public String getNameRepo() {
 		return nameRepo;
 	}
+
 	public void setNameRepo(String nameRepo) {
 		this.nameRepo = nameRepo;
 	}
+
 	public Map<String, Set<Ficheiro>> getMapFiles() {
 		return mapFiles;
 	}
+
 	public void setMapFiles(Map<String, Set<Ficheiro>> mapFiles) {
 		this.mapFiles = mapFiles;
 	}
-	public Set<Ficheiro> getVersionList(String nameFile){
+
+	public Set<Ficheiro> getVersionList(String nameFile) {
 		return mapFiles.get(nameFile);
 	}
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "RemoteRepository [onwer=" + onwer + ", timestamp=" + timestamp + ", nameRepo=" + nameRepo + "]";
+	}
+
 }
