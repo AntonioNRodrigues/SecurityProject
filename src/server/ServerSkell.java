b035b4ac681828bb130cda0f8862cb23b8a77978
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-02-27 23:53:04 +0000

    switch to deal with each operation

--- src/server/ServerSkell.java
+++ src/server/ServerSkell.java
@@ -3,36 +3,81 @@ package server;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import enums.TypeOperation;
+import enums.TypeSend;
 import message.Message;
 import message.MessageP;
 import message.MessageRS;
 import server.repository.RepositoryCatalog;
+import user.UserCatalog;
 
 public class ServerSkell {
 	private ObjectOutputStream out;
 	private ObjectInputStream in;
 	private RepositoryCatalog catRepo;
-	//private UsersCatalog catUsers;
-	
+	private UserCatalog catUsers;
+
 	public ServerSkell(ObjectOutputStream out, ObjectInputStream in) {
 		this.out = out;
-		this.in = in;	
-		catRepo = new RepositoryCatalog();
+		this.in = in;
+		this.catRepo = new RepositoryCatalog();
+		this.catUsers = new UserCatalog();
 	}
 
 	public void receiveMsg(Message msg) {
-		if(msg instanceof MessageRS){
+		if (msg instanceof MessageRS) {
 			System.out.println(msg);
-		}else if (msg instanceof MessageP){
+			TypeOperation op = ((MessageRS) msg).getTypeOperation();
+
+			switch (op) {
+			case REMOVE:
+				
+				break;
+			case SHARE:
+
+				break;
+			default:
+				break;
+			}
+
+		} else if (msg instanceof MessageP) {
 			System.out.println(msg);
-		}else if(msg instanceof Message){
+			MessageP mp = ((MessageP) msg);
+			TypeSend ts = mp.getTypeSend();
+			TypeOperation op = mp.getOperation();
+
+			switch (ts) {
+			case REPOSITORY:
+				switch (op) {
+				case PULL:
+					
+					break;
+				case PUSH:
+					
+					break;
+				default:
+					break;
+				}
+				break;
+			case FILE:
+				switch (op) {
+				case PULL:
+					
+					break;
+
+				case PUSH:
+					break;
+				default:
+					break;
+				}
+			default:
+				break;
+			}
+
+		} else if (msg instanceof Message) {
 			System.out.println(msg);
 		}
-		
+
 	}
-	
-	
-	
-	
-	
+
 }
