ce32971e568e94c6e03243f8e838604568415fe6
António Rodrigues <antonio.n.rodrigues@gmail.com>
2017-03-08 23:58:07 +0000

    Param && Constructor Changed: changed a param to repo_file_name to 2 different repo_name and file_name. We have to send always the repository name.

--- src/message/MessageP.java
+++ src/message/MessageP.java
@@ -10,14 +10,37 @@ public class MessageP extends Message implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private TypeOperation operation;
 	private TypeSend typeSend;
-	private String repo_file_name;
+	private String repoName;
+	private String fileName;
 	private int numberFiles;
 	private long timestamp;
 
-	public MessageP(User localUser, String serverAdress, String password, TypeSend typeSend, String repoFileName,
+	/**
+	 * Constructor to use when we dont want to send a file name. Using in PULL
+	 * RepoName and PUSH RepoName
+	 * 
+	 */
+	public MessageP(User localUser, String serverAdress, String password, TypeSend typeSend, String repoName,
 			TypeOperation operation, int numberFiles, long ts) {
 		super(localUser, serverAdress, password);
-		this.repo_file_name = repoFileName;
+		this.repoName = repoName;
+		this.fileName = null;
+		this.operation = operation;
+		this.typeSend = typeSend;
+		this.setNumberFiles(numberFiles);
+		this.timestamp = ts;
+	}
+
+	/**
+	 * Constructor to use when we want to send a file name. Using in PULL
+	 * fileName and PUSH FileName
+	 * 
+	 */
+	public MessageP(User localUser, String serverAdress, String password, TypeSend typeSend, String repoName,
+			String fileName, TypeOperation operation, int numberFiles, long ts) {
+		super(localUser, serverAdress, password);
+		this.repoName = repoName;
+		this.fileName = fileName;
 		this.operation = operation;
 		this.typeSend = typeSend;
 		this.setNumberFiles(numberFiles);
@@ -32,26 +55,12 @@ public class MessageP extends Message implements Serializable {
 		this.typeSend = typeSend;
 	}
 
-	public String getRepoFileName() {
-		return repo_file_name;
-	}
-
-	public void setRepoFileName(String repoFileName) {
-		this.repo_file_name = repoFileName;
-	}
-
 	public TypeOperation getOperation() {
 		return operation;
 	}
 
 	public void setOperation(TypeOperation operation) {
 		this.operation = operation;
-	}
-
-	@Override
-	public String toString() {
-		return "MessageP [User " + this.getLocalUser() + "operation=" + operation + ", typeSend=" + typeSend + ", name="
-				+ repo_file_name + "]";
 	}
 
 	public int getNumberFiles() {
@@ -68,6 +77,22 @@ public class MessageP extends Message implements Serializable {
 
 	public void setTimestamp(long timestamp) {
 		this.timestamp = timestamp;
+	}
+
+	public String getRepoName() {
+		return repoName;
+	}
+
+	public void setRepoName(String repoName) {
+		this.repoName = repoName;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
 	}
 
 }
