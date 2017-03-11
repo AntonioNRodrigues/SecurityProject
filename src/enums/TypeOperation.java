de7354d5e7ff77e9e4734b35a40465f31d8fa18d
Aqua1986 <ptpais86@gmail.com>
2017-03-11 23:27:59 +0000

    otimizado contains para O(1).

--- src/enums/TypeOperation.java
+++ src/enums/TypeOperation.java
@@ -4,11 +4,7 @@ public enum TypeOperation {
 
 	PUSH, PULL, REMOVE, SHARE, AUTH;
 
-	public static boolean contains(String operacao) {
-		for (int i = 0; i < TypeOperation.values().length; i++) {
-			if (TypeOperation.values()[i].toString().equals(operacao))
-				return true;
-		}
-		return false;
+	public static boolean contains(String op) {
+		return TypeOperation.valueOf(op).toString().equals(op);
 	}
 }
