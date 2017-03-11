3af46c21d3cf1e2e71f80b0dd2d4784fe4c17018
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-03-11 22:54:24 +0000

    added the AUTH to TypeOperation

--- src/enums/TypeOperation.java
+++ src/enums/TypeOperation.java
@@ -1,7 +1,7 @@
 package enums;
 
 public enum TypeOperation {
-	PUSH, PULL, REMOVE, SHARE;
+	PUSH, PULL, REMOVE, SHARE, AUTH;
 
 	public static boolean contains(String op) {
 		return TypeOperation.valueOf(op).equals(op);
