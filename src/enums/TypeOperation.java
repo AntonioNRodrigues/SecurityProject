1d7553978fd8407e78bce4636bc1022e7b813425
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-03-11 22:46:13 +0000

    added a method contains.

--- src/enums/TypeOperation.java
+++ src/enums/TypeOperation.java
@@ -1,5 +1,9 @@
 package enums;
 
 public enum TypeOperation {
-	PUSH, PULL, REMOVE, SHARE
+	PUSH, PULL, REMOVE, SHARE;
+
+	public static boolean contains(String op) {
+		return TypeOperation.valueOf(op).equals(op);
+	}
 }
