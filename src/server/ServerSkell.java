a2d04e0eb952ecf6888406a3406531335304168d
Aqua1986 <ptpais86@gmail.com>
2017-03-14 19:21:05 +0000

    - retirar System.out.print desnecessario

--- src/server/ServerSkell.java
+++ src/server/ServerSkell.java
@@ -118,7 +118,6 @@ public class ServerSkell {
 							rr = catRepo.buildRepo(mp.getLocalUser(),
 									mp.getRepoName());
 						}
-						out.writeObject("DONE ?!");
 						break;
 					default:
 						break;
