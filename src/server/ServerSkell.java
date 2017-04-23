f1a45477ba85aa97eaa7a1cecd33d262ff791b35
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2017-04-23 22:00:35 +0100

    SERVER: changed the name of the method

--- src/server/ServerSkell.java
+++ src/server/ServerSkell.java
@@ -134,7 +134,7 @@ public class ServerSkell {
 
 						if (!error) {
 							out.writeObject((Object) "OK");
-							rr.removeUserFromRepo(mrs.getUserId());
+							rr.removeSharedUserFromRepo(mrs.getUserId());
 						}
 					}
 
@@ -154,7 +154,7 @@ public class ServerSkell {
 						rr = catRepo.getRemRepository(mrs.getRepoName());
 
 						boolean error = false;
-						// Validar se o utilizador é dono
+						// Validar se o utilizador eh dono
 						if (!rr.getOwner().equals(mrs.getLocalUser().getName())) {
 							error = true;
 							out.writeObject((Object) "NOK");
